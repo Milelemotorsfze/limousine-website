76bd5a69c3373b048940f5d089b72bb685a6e8f4
Rejitha Rajendran <rejitha.rajendran@milele.com>
2023-10-12 14:23:42 +0400

    footer href, alt and code align

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,67 +1,71 @@
 <footer class="footer-wrapper" >
-   <div class="footer-container container">
-      <div class="footer-column three columns" id="footer-widget-1" >
-         <div id="text-5" class="widget widget_text limoking-item limoking-widget">
-            <div class="textwidget">
-             <a  href="{{route('home')}}"><img src="{{asset('/uploads/Logo/Milele Limousine - Web 1.png')}}" style="width: 170px;" alt="" /></a>
-            </div>
-</br>
-            <h3 class="limoking-widget-title">About Us</h3>
-            <div class="clear"></div>
-            <div class="textwidget">
-               <p style="margin-bottom: 1px">Cum sociis natoque penatibus et magnis dis parturient montes.</p>
-            </div>
-         </div>
-      </div>
-      <div class="footer-column three columns" id="footer-widget-2" >
-         <div id="text-9" class="widget widget_text limoking-item limoking-widget">
-            <h3 class="limoking-widget-title">Contact Info</h3>
-            <div class="clear"></div>
-            <div class="textwidget">
-               <span class="clear"></span><span class="limoking-space" style="margin-top: 10px; display: block;"></span>
-               <i class="limoking-icon fa fa-phone" style="vertical-align: middle; color: #fff; font-size: 16px; " ></i> +971-333-3333
-               <span class="clear"></span><span class="limoking-space" style="margin-top: 10px; display: block;"></span>
-               <i class="limoking-icon fa fa-mobile" style="vertical-align: middle; color: #fff; font-size: 20px; " ></i> (1)-212-333-4343
-               <span class="clear"></span><span class="limoking-space" style="margin-top: 10px; display: block;"></span>
-               <i class="limoking-icon fa fa-envelope-o" style="vertical-align: middle; color: #fff; font-size: 16px; " ></i> info@milelelamous.com
-            </div>
-         </div>
-      </div>
-      <div class="footer-column three columns" id="footer-widget-3" >
-         <div id="recent-posts-5" class="widget widget_recent_entries limoking-item limoking-widget">
-            <h3 class="limoking-widget-title">Location</h3>
-            <div class="clear"></div>
-
-             <span class="limoking-space" >Showroom 93 , Al Aweer Market ,</span>
-                <p>Ras Al Khor , United Arab Emirates</p>
-         </div>
-      </div>
-      <div class="footer-column three columns" id="footer-widget-4" >
-         <div id="gdlr-recent-portfolio2-widget-6" class="widget widget_gdlr-recent-portfolio2-widget limoking-item limoking-widget">
-            <h3 class="limoking-widget-title">Recent Works</h3>
-            <div class="clear"></div>
-            <div class="limoking-recent-port2-widget">
-               <div class="recent-port-widget-thumbnail"><a href="portfolio/night-party/index.html" ><img src="wp-content/uploads/2014/03/shutterstock_77075656-150x150.jpg" alt="" width="150" height="150" /></a></div>
-               <div class="recent-port-widget-thumbnail"><a href="portfolio/business-taxi-service/index.html" ><img src="wp-content/uploads/2014/03/shutterstock_105826964-150x150.jpg" alt="" width="150" height="150" /></a></div>
-               <div class="recent-port-widget-thumbnail"><a href="portfolio/airport-transfers/index.html" ><img src="wp-content/uploads/2014/03/shutterstock_183797051-150x150.jpg" alt="" width="150" height="150" /></a></div>
-{{--               <div class="recent-port-widget-thumbnail"><a href="portfolio/wedding-day/index.html" ><img src="wp-content/uploads/2014/03/shutterstock_71630290-150x150.jpg" alt="" width="150" height="150" /></a></div>--}}
-{{--               <div class="recent-port-widget-thumbnail"><a href="portfolio/video-inside-this-post/index.html" ><img src="wp-content/uploads/2014/03/shutterstock_258034826-150x150.jpg" alt="" width="150" height="150" /></a></div>--}}
-{{--               <div class="recent-port-widget-thumbnail"><a href="portfolio/city-tour/index.html" ><img src="wp-content/uploads/2014/03/shutterstock_142784983-150x150.jpg" alt="" width="150" height="150" /></a></div>--}}
-               <div class="clear"></div>
-            </div>
-         </div>
-      </div>
-      <div class="clear"></div>
-   </div>
-   <div class="copyright-wrapper">
-      <div class="copyright-container container">
-         <div class="copyright-left">
-            Copyright 2016 GoodLayers, All Right Reserved
-         </div>
-         <div class="copyright-right">
-            <a href="http://facebook.com/goodlayers"><i class="limoking-icon fa fa-facebook" style="vertical-align: middle;color: #bbbbbb;font-size: 20px"></i></a> <a href="http://twitter.com/goodlayers"><i class="limoking-icon fa fa-twitter" style="vertical-align: middle;color: #bbbbbb;font-size: 20px"></i></a> <a href="#"><i class="limoking-icon fa fa-dribbble" style="vertical-align: middle;color: #bbbbbb;font-size: 20px"></i></a> <a href="#"><i class="limoking-icon fa fa-pinterest" style="vertical-align: middle;color: #bbbbbb;font-size: 20px"></i></a> <a href="#"><i class="limoking-icon fa fa-google-plus" style="vertical-align: middle;color: #bbbbbb;font-size: 20px"></i></a> <a href="#"><i class="limoking-icon fa fa-instagram" style="vertical-align: middle;color: #bbbbbb;font-size: 20px"></i></a>
-         </div>
-         <div class="clear"></div>
-      </div>
-   </div>
+	<div class="footer-container container">
+		<div class="footer-column three columns" id="footer-widget-1" >
+			<div id="text-5" class="widget widget_text limoking-item limoking-widget">
+				<div class="textwidget">
+					<a  href="{{route('home')}}"><img src="{{asset('/uploads/Logo/Milele Limousine - Web 1.png')}}" style="width: 170px;" alt="logo" /></a>
+				</div>
+				</br>
+				<h3 class="limoking-widget-title">About Us</h3>
+				<div class="clear"></div>
+				<div class="textwidget">
+					<p style="margin-bottom: 1px">Cum sociis natoque penatibus et magnis dis parturient montes.</p>
+				</div>
+			</div>
+		</div>
+		<div class="footer-column three columns" id="footer-widget-2" >
+			<div id="text-9" class="widget widget_text limoking-item limoking-widget">
+				<h3 class="limoking-widget-title">Contact Info</h3>
+				<div class="clear"></div>
+				<div class="textwidget">
+					<span class="clear"></span><span class="limoking-space" style="margin-top: 10px; display: block;"></span>
+					<i class="limoking-icon fa fa-phone" style="vertical-align: middle; color: #fff; font-size: 16px; " ></i> +971-333-3333
+					<span class="clear"></span><span class="limoking-space" style="margin-top: 10px; display: block;"></span>
+					<i class="limoking-icon fa fa-mobile" style="vertical-align: middle; color: #fff; font-size: 20px; " ></i> (1)-212-333-4343
+					<span class="clear"></span><span class="limoking-space" style="margin-top: 10px; display: block;"></span>
+					<i class="limoking-icon fa fa-envelope-o" style="vertical-align: middle; color: #fff; font-size: 16px; " ></i> info@milelelamous.com
+				</div>
+			</div>
+		</div>
+		<div class="footer-column three columns" id="footer-widget-3" >
+			<div id="recent-posts-5" class="widget widget_recent_entries limoking-item limoking-widget">
+				<h3 class="limoking-widget-title">Location</h3>
+				<div class="clear"></div>
+				<span class="limoking-space" >Showroom 93 , Al Aweer Market ,</span>
+				<p>Ras Al Khor , United Arab Emirates</p>
+			</div>
+		</div>
+		<div class="footer-column three columns" id="footer-widget-4" >
+			<div id="gdlr-recent-portfolio2-widget-6" class="widget widget_gdlr-recent-portfolio2-widget limoking-item limoking-widget">
+				<h3 class="limoking-widget-title">Recent Works</h3>
+				<div class="clear"></div>
+				<div class="limoking-recent-port2-widget">
+					<div class="recent-port-widget-thumbnail"><a href="" ><img src="wp-content/uploads/2014/03/shutterstock_77075656-150x150.jpg" alt="" width="150" height="150" /></a></div>
+					<div class="recent-port-widget-thumbnail"><a href="" ><img src="wp-content/uploads/2014/03/shutterstock_105826964-150x150.jpg" alt="" width="150" height="150" /></a></div>
+					<div class="recent-port-widget-thumbnail"><a href="" ><img src="wp-content/uploads/2014/03/shutterstock_183797051-150x150.jpg" alt="" width="150" height="150" /></a></div>
+					<div class="recent-port-widget-thumbnail"><a href="" ><img src="wp-content/uploads/2014/03/shutterstock_71630290-150x150.jpg" alt="" width="150" height="150" /></a></div>
+					<div class="recent-port-widget-thumbnail"><a href="" ><img src="wp-content/uploads/2014/03/shutterstock_258034826-150x150.jpg" alt="" width="150" height="150" /></a></div>
+					<div class="recent-port-widget-thumbnail"><a href="" ><img src="wp-content/uploads/2014/03/shutterstock_142784983-150x150.jpg" alt="" width="150" height="150" /></a></div>
+					<div class="clear"></div>
+				</div>
+			</div>
+		</div>
+		<div class="clear"></div>
+	</div>
+	<div class="copyright-wrapper">
+		<div class="copyright-container container">
+			<div class="copyright-left">
+				Copyright 2023 Milele Limousine
+			</div>
+			<div class="copyright-right">
+				<a href=""><i class="limoking-icon fa fa-facebook" style="vertical-align: middle;color: #bbbbbb;font-size: 20px"></i></a> 
+				<a href=""><i class="limoking-icon fa fa-twitter" style="vertical-align: middle;color: #bbbbbb;font-size: 20px"></i></a> 
+				<a href="#"><i class="limoking-icon fa fa-dribbble" style="vertical-align: middle;color: #bbbbbb;font-size: 20px"></i></a> 
+				<a href="#"><i class="limoking-icon fa fa-pinterest" style="vertical-align: middle;color: #bbbbbb;font-size: 20px"></i></a>
+				<a href="#"><i class="limoking-icon fa fa-google-plus" style="vertical-align: middle;color: #bbbbbb;font-size: 20px"></i></a>
+				<a href="#"><i class="limoking-icon fa fa-instagram" style="vertical-align: middle;color: #bbbbbb;font-size: 20px"></i></a>
+			</div>
+			<div class="clear"></div>
+		</div>
+	</div>
 </footer>
