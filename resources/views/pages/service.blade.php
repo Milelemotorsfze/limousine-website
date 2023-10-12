5037b28f2a6f2df89e7a47f3fe6b737c84a06f7e
Rejitha Rajendran <rejitha.rajendran@milele.com>
2023-10-12 15:00:38 +0400

    services -  paragraph added

--- resources/views/pages/service.blade.php
+++ resources/views/pages/service.blade.php
@@ -26,7 +26,7 @@
 								<div class="limoking-item-title-center-icon-divider"></div>
 							</div>
 						</div>
-						<div class="limoking-item-title-caption limoking-skin-info">We are the most popular limousine service in New York.</div>
+						<div class="limoking-item-title-caption limoking-skin-info">We are the most popular limousine service in United Arab Emirates.</div>
 					</div>
 				</div>
 			</div>
